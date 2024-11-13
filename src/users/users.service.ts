12316a1c1273e1b12ca1ea1e29fbf867077751fc
Henrique Torres <henriquetlandin@gmail.com>
2024-11-13 10:59:32 -0300

    feat: adicionando o modulo de autenticação

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -14,4 +14,8 @@ export class UsersService {
     newUser.password = bcryptHashSync(newUser.password, 10);
     this.users.push(newUser);
   }
+
+  findByUsername(username: string): UserDto | null {
+    return this.users.find((user) => user.username == username);
+  }
 }
