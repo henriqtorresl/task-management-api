89a0256d1839df5f87f2d46f4f629a464951f720
Henrique Torres <henriquetlandin@gmail.com>
2024-11-13 10:15:16 -0300

    feat: adicionando modulo de usuarios

---
+++ src/users/users.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { UserDto } from './users.dto';
+import { v4 as uuid } from 'uuid';
+import { hashSync as bcryptHashSync } from 'bcrypt';
+
+@Injectable()
+export class UsersService {
+  private readonly users: UserDto[] = [];
+
+  constructor() {}
+
+  create(newUser: UserDto) {
+    newUser.id = uuid();
+    newUser.password = bcryptHashSync(newUser.password, 10);
+    this.users.push(newUser);
+  }
+}
